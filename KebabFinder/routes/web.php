f6a740bcbd4f99b3d44f259d8613cae25b032c2a
Eimantas Dima <73961810+LynJuice@users.noreply.github.com>
2023-09-07 11:02:58 +0300

    Spatie

--- KebabFinder/routes/web.php
+++ KebabFinder/routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/index', function () {
     return view('index');
 })->name('index');
 
