77d8fe16fee95b7297258a760ba757b55d60b961
Eimantas Dima <73961810+LynJuice@users.noreply.github.com>
2023-09-13 12:07:11 +0300

    Bandyta pajungti vartotoju roles ir leidimus

--- KebabFinder/database/factories/UserFactory.php
+++ KebabFinder/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Spatie\Permission\Traits\HasRoles;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
