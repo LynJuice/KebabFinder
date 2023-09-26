ee04d21670995c8ae5f1aa95072bd70c6f74e4af
DovydasU <ulonasd@gmail.com>
2023-09-26 13:48:54 +0300

    sukurti kebab_produktai, komentarai, produktai modeliai

---
+++ KebabFinder/app/Http/Controllers/KebabProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kebab_Product;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreKebab_ProductRequest;
+use App\Http\Requests\UpdateKebab_ProductRequest;
+
+class KebabProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKebab_ProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kebab_Product $kebab_Product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kebab_Product $kebab_Product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKebab_ProductRequest $request, Kebab_Product $kebab_Product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kebab_Product $kebab_Product)
+    {
+        //
+    }
+}
---
+++ KebabFinder/database/factories/KebabProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Kebab_Product>
+ */
+class KebabProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
