5a850bf2e03813928b458a2c1af015d1fa162724
Eimantas Dima <73961810+LynJuice@users.noreply.github.com>
2023-09-27 15:16:11 +0300

    Kebab pridejimas

--- KebabFinder/routes/web.php
+++ KebabFinder/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KebabShopController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/kebabAdd', [KebabShopController::class, 'create'])->name('kebabShop.create');
+    Route::post('/kebabStore', [KebabShopController::class, 'store'])->name('kebabShop.store');
 });
 
 Route::get('/logout', function () {
