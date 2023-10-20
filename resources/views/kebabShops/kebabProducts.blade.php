7cbcaac00960d34692c69009016a97f1f046c0e9
NojusD <118273394+NojusD@users.noreply.github.com>
2023-10-20 17:13:39 +0300

    KebabProducts

--- resources/views/kebabShops/kebabProducts.blade.php
+++ resources/views/kebabShops/kebabProducts.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.admin_base')
 
-@section('title', 'Kebab Produktai')
+@section('title', 'Produktai')
 
 @section('content')
-
-{{ $kebab_products }}
-<br>
-<br>
-<br>
-{{ $user_products }}
-
-
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Kebab Produktai</h2>
+            <h2> Produktai</h2>
         </div>
         <!-- <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('table') }}"> Back</a>
@@ -28,14 +20,16 @@
             <table class="table">
                 <thead>
                     <tr>
+                        <th>ID</th>
                         <th>Pavadinimas</th>
                         <th>Atsiliepimai</th>
                         <th>Kebabinės</th>
                     </tr>
                 </thead>
                 <tbody class="table-border-bottom-0">
+                    @foreach ($user_products as $product)
                     <tr>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>NAME HERE</strong></td>
+                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>{{ $product->name }}</strong></td>
                         <td>
                             <div class="progress mb-3">
                                 <div class="progress-bar" role="progressbar" aria-valuenow="47" aria-valuemin="0" aria-valuemax="100">
@@ -45,6 +39,9 @@
                         </td>
                         <td>
 
+                            @foreach ($product->kebabShops as $kebabShop)
+                            <div> {{ $kebabShop->id }} - {{$kebabShop->name}} </div>
+                            @endforeach
                         </td>
                         <td>
                             <div class="dropdown">
@@ -56,6 +53,7 @@
                             </div>
                         </td>
                     </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
