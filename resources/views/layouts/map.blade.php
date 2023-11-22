1edc021eecc8b245491f87c82bf30f62fdaca9eb
Vilius Ž <109176033+Maraderk@users.noreply.github.com>
2023-11-22 13:45:57 +0200

    Update map.blade.php

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -13,46 +13,72 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let marker, circle, zoomed;
 
+var yellowIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 navigator.geolocation.watchPosition(success, error);
 
 function success(pos) {
 
  const lat = pos.coords.latitude;
  const lng = pos.coords.longitude;
- const accuracy = pos.coords.accuracy;
-
- if (marker) {
-     map.removeLayer(marker);
-     map.removeLayer(circle);
- }
- // Removes any existing marker and circule (new ones about to be set)
-
- marker = L.marker([lat, lng]).addTo(map);
- circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
- // Adds marker to the map and a circle for accuracy
-
- if (!zoomed) {
-     zoomed = map.fitBounds(circle.getBounds()); 
- }
- // Set zoom to boundaries of accuracy circle
-
- map.setView([lat, lng]);
- // Set map focus to current user position
-
-@foreach ($kebab_map as $kebab)
-    L.marker([{{ $kebab->latitude }}, {{ $kebab->longitude }}])
-    .addTo(map)
-    .bindPopup("<b>{{ $kebab->name }}</b><br><button onclick=\"window.location.href='{{ route('shops.show', $kebab->id) }}'\">View</button>");
-@endforeach
+ DisplayMap(lat, lng);
 }
 
 function error(err) {
-
  if (err.code === 1) {
      alert("Please allow geolocation access");
  } else {
      alert("Cannot get current location");
  }
+}
 
+function DisplayMap(lat, lng) {
+    const accuracy = pos.coords.accuracy / 5;
+
+    if (marker) {
+        map.removeLayer(marker);
+        map.removeLayer(circle);
+    }
+// Removes any existing marker and circule (new ones about to be set)
+
+marker = L.marker( new L.LatLng(lat, lng), {icon:yellowIcon}).addTo(map);
+circle = L.circle([lat, lng], { radius: accuracy, color: 'DarkRed', fillColor: 'Red' }).addTo(map);
+// Adds marker to the map and a circle for accuracy
+
+if (!zoomed) {
+    zoomed = map.fitBounds(circle.getBounds()); 
+}
+// Set zoom to boundaries of accuracy circle
+
+map.setView([lat, lng]);
+// Set map focus to current user position
+
+
+
+@foreach ($kebab_map as $kebab)
+
+var LogoIcon = new L.Icon({
+ iconUrl: '{{ asset('images/diners/logos/' . $kebab->logo) }}',
+ shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+ iconSize: [40, 40],
+ iconAnchor: [12, 41],
+ popupAnchor: [1, -34],
+ shadowSize: [41, 41]
+});
+
+
+
+   L.marker(  new L.LatLng({{ $kebab->latitude }}, {{ $kebab->longitude }}), {icon:LogoIcon})
+   .addTo(map)
+   .bindPopup("<b>{{ $kebab->name }}</b><br><button onclick=\"window.location.href='{{ route('shops.show', $kebab->id) }}'\">View</button>");
+   
+@endforeach
 }
  </script>
